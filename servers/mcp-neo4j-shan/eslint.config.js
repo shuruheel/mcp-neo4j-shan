312f8b69aa84ea1ac31bcb6a5d95cbc76a7b543b
Shuruheel <shan@rizvi.nu>
2025-02-24 18:22:30 -0500

    Fix config files

--- servers/mcp-neo4j-shan/eslint.config.js
+++
@@ -1,4 +0,0 @@
-export default [{
-  extends: ['../../eslint.config.js'],
-  ignorePatterns: ['!**/*'],
-}];
--- servers/mcp-neo4j-shan/jest.config.ts
+++ servers/mcp-neo4j-shan/jest.config.ts
@@ -1,17 +1,10 @@
-/* eslint-disable */
 export default {
-  displayName: 'mcp-neo4j-shan',
+  displayName: 'mcp-neo4j-memory',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  coverageDirectory: '../../coverage/servers/mcp-neo4j-shan',
+  coverageDirectory: '../../coverage/servers/mcp-neo4j-memory',
 };
